axxx007xxxz/citrus_frameworks_native

dumpstate: add Citrus-CAF version to output
Change-Id: Ibd804872d7d32ac1d26efaf30927a2359d2da287

cmds/dumpstate/dumpstate.cpp
@@ -538,7 +538,9 @@ static void print_header(std::string version) {
     char build[PROPERTY_VALUE_MAX], fingerprint[PROPERTY_VALUE_MAX];
     char radio[PROPERTY_VALUE_MAX], bootloader[PROPERTY_VALUE_MAX];
     char network[PROPERTY_VALUE_MAX], date[80];
+    char citrus_version[PROPERTY_VALUE_MAX];
 
+    property_get("ro.citrus.version", citrus_version, "(unknown)");
     property_get("ro.build.display.id", build, "(unknown)");
     property_get("ro.build.fingerprint", fingerprint, "(unknown)");
     property_get("ro.build.type", build_type, "(unknown)");
@@ -553,6 +555,7 @@ static void print_header(std::string version) {
 
     printf("\n");
     printf("Build: %s\n", build);
+    printf("Citru-CAF version: %s\n", citrus_version);
     printf("Build fingerprint: '%s'\n", fingerprint); /* format is important for other tools */
     printf("Bootloader: %s\n", bootloader);
     printf("Radio: %s\n", radio);

Citrus-CAF/frameworks_native

cmds/dumpstate/dumpstate.cpp
@@ -747,7 +747,9 @@ static void print_header(std::string version) {
     char build[PROPERTY_VALUE_MAX], fingerprint[PROPERTY_VALUE_MAX];
     char radio[PROPERTY_VALUE_MAX], bootloader[PROPERTY_VALUE_MAX];
     char network[PROPERTY_VALUE_MAX], date[80];
+    char citrus_version[PROPERTY_VALUE_MAX];
 
+    property_get("ro.citrus.version", citrus_version, "(unknown)");
     property_get("ro.build.display.id", build, "(unknown)");
     property_get("ro.build.fingerprint", fingerprint, "(unknown)");
     property_get("ro.build.type", build_type, "(unknown)");
@@ -762,6 +764,7 @@ static void print_header(std::string version) {
 
     printf("\n");
     printf("Build: %s\n", build);
+    printf("Citrus-CAF version: %s\n", citrus_version);
     printf("Build fingerprint: '%s'\n", fingerprint); /* format is important for other tools */
     printf("Bootloader: %s\n", bootloader);
     printf("Radio: %s\n", radio);